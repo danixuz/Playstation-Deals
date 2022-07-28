daac8daa545690c79d695cf7c4e49d1d616f63bb
danixuz <danixuz@icloud.com>
2022-07-29 00:30:14 +0300

    Initial Commit

---
+++ Playstation-Deals/Playstation_DealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Playstation_DealsApp.swift
+//  Playstation-Deals
+//
+//  Created by Daniel Spalek on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Playstation_DealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
